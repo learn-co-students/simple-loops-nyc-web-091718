d0fa5a50c6a26ac2652a4c8b30736f0ec87b7a93
Asaf Davidov <adavidov@conncoll.edu>
2018-09-05 20:40:24 +0000

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -55,6 +55,7 @@ def for_iterator(number_of_times)
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
   for number_of_times in 1..number_of_times
+  
     puts phrase
   end
 end
